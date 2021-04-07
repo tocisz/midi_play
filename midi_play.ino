98a3997e2e3b471acc4547f25436ce62c3ed2b1c
Tomasz Cichocki <cichymail@gmail.com>
2021-04-07 23:02:38 +0200

    not needed

--- midi_play.ino
+++ midi_play.ino
@@ -8,11 +8,9 @@
  *  D15 - output - ADC BCK
  *  D13 - input  - software serial: MIDI
  *  (so we can't use hardware Serial - both normal and alternative ports are taken)
+ *  (Serial1 output is taken too)
  *  D1  - ouput  - software serial: console
  */
-
-String inputString = "";
-bool stringComplete = false;
 
 SoftwareSerial swSer;
 
@@ -23,25 +21,12 @@ void setup() {
   // INIT serial communication
   Serial.begin(115200);
   Serial.println('\n');
-  inputString.reserve(100);
 
   pinMode(13, INPUT); // soft serial
   swSer.begin(31250, SWSERIAL_8N1, 13);
 }
 
 void loop() {
-  // check serial port events
-  if (Serial.available() > 0) {
-    serialEvent();
-  }
-
-  // serial port commands handling
-  if (stringComplete) {
-    Serial.println(inputString);
-    inputString = "";
-    stringComplete = false;
-  }
-
   while (swSer.available() > 0) {
     char c = swSer.read();
     if (c < 0xf) {
@@ -49,20 +34,5 @@ void loop() {
     }
     Serial.print(swSer.read(), HEX);
     yield();
-  }
-}
-
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
-    } else {
-      // add it to the inputString:
-      inputString += inChar;
-    }
   }
 }
